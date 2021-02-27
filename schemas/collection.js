0ad440838cc42a390755913b0446a8afd0a48dde
Jon Meyers <jonathon.d.meyers@gmail.com>
2021-02-27 22:14:42 +1100

    add basic types for sanity cms

---
+++ schemas/collection.js
@@ -0,0 +1,60 @@
+export default {
+  name: "collection",
+  title: "Collection",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+      },
+    },
+    {
+      name: "seoDescription",
+      title: "SEO description",
+      type: "string",
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "tags",
+    },
+    {
+      name: "isPublished",
+      title: "Published",
+      type: "boolean",
+    },
+    {
+      name: "posts",
+      title: "Posts",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "post" }] }],
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      return selection;
+    },
+  },
+};
---
+++ schemas/post.js
@@ -0,0 +1,57 @@
+import client from "part:@sanity/base/client";
+
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "collection",
+      title: "Collection",
+      type: "reference",
+      to: [{ type: "collection" }],
+      weak: false,
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        slugify: (str) => str.replace(" ", "-").toLowerCase(),
+        source: async (doc) => {
+          if (!doc.collection) {
+            return doc.title;
+          }
+
+          const collection = await client.getDocument(doc.collection._ref);
+          return `${collection.slug.current}/${doc.title}`;
+        },
+      },
+    },
+    {
+      name: "numInCollection",
+      title: "Number of Post in Collection",
+      type: "number",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      return selection;
+    },
+  },
+};
